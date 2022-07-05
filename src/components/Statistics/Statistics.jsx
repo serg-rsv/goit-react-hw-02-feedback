a60776cd8c490fefb60817dfba8819d3ef413d8e
Serhii Rybak <rybaksw@gmail.com>
2022-07-05 16:46:51 +0300

    fix(Statistics): add white space to Neutral count

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -17,7 +17,7 @@ export const Statistics = ({
   return (
     <>
       <StatField>Good: {good}</StatField>
-      <StatField>Neutral:{neutral}</StatField>
+      <StatField>Neutral: {neutral}</StatField>
       <StatField>Bad: {bad}</StatField>
       <StatField>Total: {total}</StatField>
       <StatField>Positive feedback: {positivePercentage}%</StatField>
